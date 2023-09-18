a84843a435992faae199902ebba2d15305b2d9a3
SangKeun Nam <sangkeunnam@SangKeunnoMacBook-Air.local>
2023-09-18 22:10:50 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>To-Do-List</h2>
+    <TodoSimpleForm @add-todo="addTodo" />
+    <div v-if="!todos.length">추가된 Todo가 없습니다</div>
+    <!-- :todos 바인딩으로 자식 컴포넌트인 TodoList에 todos를 넘겨준다 -->
+    <TodoList :todos="todos" @toggle-todo="toggletodo" />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 컴포넌트 불러오기 .. 객체 명은 내 마음대로 작성가능하지만, 가능하면 vue파일 이름으로 통일하자
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoSimpleForm,
+    TodoList,
+  },
+
+  setup() {
+    const todos = ref([]);
+
+    const todoStyle = {
+      textDecoration: 'line-through',
+      color: 'gary',
+    };
+
+    // TodoSimpleForm컴포넌트에서 emit해준 객체를 파라메터로 해서 todos배열에 추가해준다
+    const addTodo = (todo) => {
+      console.log(todo);
+      todos.value.push(todo);
+    };
+    const deletetodo = (index) => {
+      // 받아온 인덱스를 splice함수를 이용해 삭제해준다.
+      todos.value.splice(index, 1);
+    };
+
+    const toggletodo = (index) => {
+      console.log(index);
+    };
+    return {
+      todoStyle,
+      addTodo,
+      todos,
+      deletetodo,
+      toggletodo,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.Error {
+  color: red;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="card mt-2"
+    v-for="(t, index) in todos"
+    :key="t.id"
+  >
+    <div class="card-body p-2 d-flex align-items-center">
+      <div class="form-check flex-grow-1">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          :value="t.completed"
+          @change="toggleTodo(index)"
+        />
+        <!--t.completed가 참이면 todoStyle를 , 거짓이면 {}를 실행-->
+        <label
+          class="form-check-label"
+          :class="{ todoSt: t.completed }"
+        >
+          {{ t.subject }}
+        </label>
+      </div>
+      <div>
+        <button
+          class="btn btn-danger btn-sm"
+          @click="deletetodo(index)"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const toggleTodo = (index) => {
+      context.emit('toggle-todo', index);
+    };
+    return {
+      toggleTodo,
+    };
+  },
+};
+</script>
+
+<style>
+.todoSt {
+  color: gray;
+  text-decoration: line-through;
+}
+</style>
